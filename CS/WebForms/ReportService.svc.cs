ff0653af443185507da7d1b9204af1657f58943a
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 22:40:31 +0400

    Change example status to out of maintenance

--- CS/WebForms/ReportService.svc.cs
+++
@@ -1,8 +0,0 @@
-﻿using DevExpress.Data.Utils.ServiceModel;
-
-namespace WebForms {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ReportService" in code, svc and config file together.
-    [SilverlightFaultBehavior]
-    public class ReportService : DevExpress.XtraReports.Service.ReportService {
-    }
-}
